041f9349f63759940daeab94f9ebb91a9870eec2
pc <zoyanoor2003@gmail.com>
2022-11-06 17:14:26 +0530

    Pushing first file

---
+++ hello world.py
@@ -0,0 +1,38 @@
+print("hello\nworld")
+print("hello \bworld")
+print("hello\rworld")
+print("Hello\tworld")
+''''hhhh
+hhhh
+jhjhj'''
+''' hello
+'''
+#print
+'''python'''
+#declaring a single integer variable
+integervalue=10
+print(integervalue)
+floatvalue=0.5
+print(floatvalue)
+stringvalue="hello"
+print(stringvalue)
+stringvalue="35"
+print(stringvalue)
+booleanvalue=True
+print(booleanvalue)
+# usage of type function
+print(type(integervalue))
+print(type(floatvalue))
+print(type(stringvalue))
+print(type(booleanvalue))
+#assigning multiple values in one line
+integervalue,floatvalue,stringvalue,booleanvalue=10,0.5,"hello",False
+print(integervalue,floatvalue,stringvalue,booleanvalue)
+#using semicolon
+integervalue=15;print(integervalue)
+integervalue="hi";print(integervalue)
+print(type(integervalue))
+#using input function
+inputvalue=input("Enter a value:")
+print(inputvalue)
+
