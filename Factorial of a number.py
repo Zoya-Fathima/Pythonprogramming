12a80e7827baa00838769d4399a380afe20f1a09
Zoya-Fathima <117572490+Zoya-Fathima@users.noreply.github.com>
2022-11-13 18:18:56 +0530

    Add files via upload

---
+++ Factorial of a number.py
@@ -0,0 +1,11 @@
+print("Factorial of a Number")
+num=int(input("Enter a Number: "))
+factorial=1
+if num in range(-1,0):
+    print("factorial doesn't exist for negative numbers ")
+elif num==0:
+    print("The factorial of zero is 1 ")
+else:
+    for i in range(1,num+1):
+        factorial=factorial*i
+        print("The factorial of",num,"is",factorial)
