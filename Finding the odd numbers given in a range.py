64cb3bc983745f94e89e93d9132ce89a4408604c
Zoya-Fathima <117572490+Zoya-Fathima@users.noreply.github.com>
2022-11-13 19:00:05 +0530

    Add files via upload

---
+++ Finding the odd numbers given in a range.py
@@ -0,0 +1,6 @@
+start_point=int(input("Enter the start point:"))
+end_point=int(input("Enter the end point"))
+print("The odd numbers between" , start_point ,"and" , end_point , "is:")
+for i in range(start_point,end_point+1):
+    if i%2!=0:
+        print(i ,end=" ")
