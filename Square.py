48124e42262464d6188d257d5854240e650b42c5
Zoya-Fathima <117572490+Zoya-Fathima@users.noreply.github.com>
2022-11-14 20:03:51 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,12 @@
+start_point=int(input("Enter the start point:"))
+end_point=int(input("Enter the end point:"))
+n=1
+sum=0
+
+while n<=end_point:
+    sum=sum+n*n
+    avg=sum/((end_point+1)-start_point)
+    print("Square of ", n , "is" , n*n)
+    n=n+1
+    print("sum is",sum)
+    print("avg is",avg)
